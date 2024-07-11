81785c33724fc017b861a9baa0d0915df2d9d4d5
Fábio <fabio.oliveira@poscontrole.com.br>
2024-07-11 15:37:26 -0300

    Página de Login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,8 +3,19 @@
 import Link from "next/link";
 import Navbar from "@/components/Navbars/AuthNavbar.js";
 import FooterSmall from "@/components/Footers/FooterSmall.js";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Login() {
+  const { push } = useRouter();
+
+  const [message, setMessage] = useState<string>("");
+
+  function btnLoginClick(): void {
+    push("/register");
+  }
+
   return (
     <>
       <Navbar transparent />
@@ -21,108 +32,39 @@ export default function Login() {
               <div className="w-full lg:w-4/12 px-4">
                 <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
                   <div className="rounded-t mb-0 px-6 py-6">
+                    <div className="flex content-center items-center justify-center mb-5">
+                      <Image
+                        src={"/img/cerberus.png"}
+                        width={128}
+                        height={128}
+                        alt="Cerberus"
+                      />
+                    </div>
                     <div className="text-center mb-3">
                       <h6 className="text-blueGray-500 text-sm font-bold">
-                        Sign in with
+                        Sign in with your MetaMask and start bot trading today.
                       </h6>
-                    </div>
-                    <div className="btn-wrapper text-center">
                       <button
-                        className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
+                        className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 mt-5 inline-flex items-center"
                         type="button"
+                        onClick={btnLoginClick}
                       >
-                        <img
-                          alt="..."
-                          className="w-5 mr-1"
-                          src="/img/github.svg"
+                        <Image
+                          src="/img/metamask.svg"
+                          width={64}
+                          height={64}
+                          alt="MetaMask"
                         />
-                        Github
+                        <span>Click to Connect</span>
                       </button>
-                      <button
-                        className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                        type="button"
-                      >
-                        <img
-                          alt="..."
-                          className="w-5 mr-1"
-                          src="/img/google.svg"
-                        />
-                        Google
-                      </button>
+                      <div>{message}</div>
                     </div>
-                    <hr className="mt-6 border-b-1 border-blueGray-300" />
-                  </div>
-                  <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                    <div className="text-blueGray-400 text-center mb-3 font-bold">
-                      <small>Or sign in with credentials</small>
-                    </div>
-                    <form>
-                      <div className="relative w-full mb-3">
-                        <label
-                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                          htmlFor="grid-password"
-                        >
-                          Email
-                        </label>
-                        <input
-                          type="email"
-                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                          placeholder="Email"
-                        />
-                      </div>
-
-                      <div className="relative w-full mb-3">
-                        <label
-                          className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                          htmlFor="grid-password"
-                        >
-                          Password
-                        </label>
-                        <input
-                          type="password"
-                          className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                          placeholder="Password"
-                        />
-                      </div>
-                      <div>
-                        <label className="inline-flex items-center cursor-pointer">
-                          <input
-                            id="customCheckLogin"
-                            type="checkbox"
-                            className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
-                          />
-                          <span className="ml-2 text-sm font-semibold text-blueGray-600">
-                            Remember me
-                          </span>
-                        </label>
-                      </div>
-
-                      <div className="text-center mt-6">
-                        <button
-                          className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                          type="button"
-                        >
-                          Sign In
-                        </button>
-                      </div>
-                    </form>
                   </div>
                 </div>
                 <div className="flex flex-wrap mt-6 relative">
-                  <div className="w-1/2">
-                    <a
-                      href="#pablo"
-                      onClick={(e) => e.preventDefault()}
-                      className="text-blueGray-200"
-                    >
-                      <small>Forgot password?</small>
-                    </a>
-                  </div>
-                  <div className="w-1/2 text-right">
-                    <Link href="/auth/register" className="text-blueGray-200">
-                      <small>Create new account</small>
-                    </Link>
-                  </div>
+                  <Link href="/register" className="text-blueGray-200">
+                    <small>Create new account</small>
+                  </Link>
                 </div>
               </div>
             </div>
