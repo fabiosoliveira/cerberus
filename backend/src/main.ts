7c8d2cd6b752c0bdcda5c8ddbbdedc32353ac544
Fábio <fabio.oliveira@poscontrole.com.br>
2024-07-17 09:01:33 -0300

    Arquitetura do NestJS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import Config from './config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(Config.PORT);
+
+  app.enableCors({
+    origin: Config.CORS_ORIGIN,
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(Config.PORT, () =>
+    console.log(`App listening on port ${Config.PORT}`),
+  );
 }
 bootstrap();
