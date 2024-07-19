b38fdf986e12db0b38ab924fd86a518fd73a3046
Fábio <fabio.oliveira@poscontrole.com.br>
2024-07-19 10:49:29 -0300

    Testes e Cobertura do AuthService

---
+++ backend/test/auth/auth.service.spec.ts
@@ -0,0 +1,43 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { jwtServiceMock } from './jwt.service.mock';
+import { AuthService } from '../../src/auth/auth.service';
+import { jwtMock } from './auth.service.mock';
+import { UnauthorizedException } from '@nestjs/common';
+
+describe('AuthService Tests', () => {
+  let authService: AuthService;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      providers: [AuthService, jwtServiceMock],
+    }).compile();
+
+    authService = moduleFixture.get<AuthService>(AuthService);
+  });
+
+  it('Should be defined', () => {
+    expect(authService).toBeDefined();
+  });
+
+  it('Should create token', async () => {
+    const result = await authService.createToken(jwtMock);
+    expect(result).toBeTruthy();
+  });
+
+  it('Should decode token', () => {
+    const result = authService.decodeToken('abc123');
+    expect(result).toBeDefined();
+    expect(result.userId).toEqual(jwtMock.userId);
+  });
+
+  it('Should check token', async () => {
+    const result = await authService.checkToken('abc123');
+    expect(result).toBeTruthy();
+  });
+
+  it('Should NOT check token', async () => {
+    await expect(authService.checkToken(null!)).rejects.toEqual(
+      new UnauthorizedException('Invalid JWT.'),
+    );
+  });
+});
