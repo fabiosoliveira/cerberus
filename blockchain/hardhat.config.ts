c5366a54b8d1957a43bcdefe9a9e352774b99d8e
Fábio <fabio.oliveira@poscontrole.com.br>
2024-07-23 11:51:54 -0300

    Testes e Deploy do CerberusPay

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,8 +1,30 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.23",
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: process.env.INFURA_URL,
+      chainId: parseInt(`${process.env.CHAIN_ID}`),
+      accounts: {
+        mnemonic: process.env.SECRET,
+      },
+    },
+    local: {
+      chainId: 31337,
+      url: "http://127.0.0.1:8545",
+      accounts: {
+        mnemonic: "test test test test test test test test test test test junk",
+      },
+    },
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  },
 };
 
 export default config;
