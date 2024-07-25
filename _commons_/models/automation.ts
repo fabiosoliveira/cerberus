ba36b763b1124fe714dfec43178d91e495690f95
Fábio <fabio.oliveira@poscontrole.com.br>
2024-07-25 14:28:14 -0300

    Ajustes e AutomationService

---
+++ _commons_/models/automation.ts
@@ -0,0 +1,40 @@
+import { ChainId } from "./chainId";
+import { Exchange } from "./exchange";
+
+export type Condition = {
+  field: string;
+  operator: string;
+  value: string;
+};
+
+export default class Automation {
+  id?: string;
+  userId: string;
+  name: string;
+  exchange: Exchange;
+  network: ChainId;
+  poolId: string | null;
+  openCondition: Condition;
+  closeCondition: Condition | null;
+  isOpened: boolean;
+  isActive: boolean;
+  nextAmount: string;
+  pnl?: number;
+  tradeCount?: number;
+
+  constructor(automation: Automation) {
+    this.id = automation.id;
+    this.userId = automation.userId;
+    this.exchange = automation.exchange || Exchange.Uniswap;
+    this.network = automation.network || ChainId.MAINNET;
+    this.name = automation.name;
+    this.poolId = automation.poolId;
+    this.openCondition = automation.openCondition;
+    this.closeCondition = automation.closeCondition;
+    this.isOpened = automation.isOpened === true;
+    this.isActive = automation.isActive === true;
+    this.nextAmount = automation.nextAmount;
+    this.pnl = automation.pnl || 0;
+    this.tradeCount = automation.tradeCount || 0;
+  }
+}
