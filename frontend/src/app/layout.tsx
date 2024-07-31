0e6f5a5bda46fe72cb038d0db93ee7d0f5a0982e
Fábio <fabio.oliveira@poscontrole.com.br>
2024-07-31 11:06:28 -0300

    Componente Toast

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,19 +1,19 @@
 import "../styles/globals.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "../styles/tailwind.css";
+
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Cerberus Plataform",
-  description: "Web3 Bot trading platform",
-  themeColor: "#000000",
+  title: "Cerberus Platform",
+  description: "Web3 Bot trading platform.",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <head>
@@ -23,6 +23,9 @@ export default function RootLayout({
           sizes="76x76"
           href="/img/brand/apple-icon.png"
         />
+        <link type="text/css" href="/css/notyf.min.css" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
       </head>
       <body className="text-blueGray-700 antialiased">
         {children}
@@ -30,6 +33,7 @@ export default function RootLayout({
           type="text/javascript"
           src="https://s3.tradingview.com/tv.js"
         ></script>
+        <script type="text/javascript" src="/js/notyf.min.js"></script>
       </body>
     </html>
   );
